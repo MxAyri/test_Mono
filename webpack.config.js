4a35220a7c2a72c623d9d7cba9e18bf813888e42
MxAyri <79870485+MxAyri@users.noreply.github.com>
2024-12-12 17:21:09 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Точка входа для сборки проекта
+
+  output: {
+    filename: 'bundle.js', // Имя выходного файла сборки
+    path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ],
+      },
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    },
+    watchFiles: ["./src/*"],
+    open: true, // Автоматически открывать браузер
+    hot: true,
+  },
+
+  mode: 'development', // Режим сборки
+};
+
